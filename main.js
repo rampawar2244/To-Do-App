97361c94c2ce23f09a0dcabd63c6836ef932548f
html_demo <sonnakulaswathi@gmail.com>
2022-05-08 17:32:54 +0530

    second

--- main.js
+++ main.js
@@ -9,14 +9,7 @@ submitBtn.addEventListener('click', e => {
 
     const time = document.querySelector('#task-time').value
     // console.log(time);
-    const newLi = document.createElement('li')
-    // Adding the CSS style class as same as the other li
-    newLi.setAttribute('class', 'list-group-item')
-    // Appending the text value to the created li
-    newLi.textContent = name
-    // Appending the created li to the ul in DOM
-    // document.querySelector('ul').appendChild(newLi)
-    console.log(newLi)
+   
 
     
 
